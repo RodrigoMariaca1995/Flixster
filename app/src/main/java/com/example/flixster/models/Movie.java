315b2bb2f068f2d0a9a86a9d8ffd446a7f9f1938
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-09-09 15:00:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,46 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path"); //gets string "backdrop_path" from movie API and assigns it to backdropPath
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException { //creates a list of movies from the array of movies in the API
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() { //image loading library is used because returning posterPath would return the link in the API rather than the image.
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
